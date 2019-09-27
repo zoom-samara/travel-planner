0e3935ff0a52cdf8805b494fa7161ca7a95ac646
Evgeniy Bulgakov <zoom81@yandex.ru>
2019-09-27 16:45:27 +0400

    Add base styles

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Landing: React.FC = () => (
-  <div>
-    Landing
-    <br />
-    <Link to="/auth/signin">Login page</Link>
-    <Link to="/auth/signup">Registration page</Link>
-    <Link to="/service/trips">Trips List</Link>
+  <div className="container landing">
+    <div className="landing_photos">
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/0.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/1.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/2.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/3.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/4.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/5.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/6.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/7.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/8.jpg')} alt=""/></div>
+      <div className="landing_photo-wrapper"><img className="landing_photo" src={require('./images/9.jpg')} alt=""/></div>
+    </div>
+    <Link to="/auth/signin" className="landing_btn">
+      Join to us
+    </Link>
   </div>
 )
 
--- src/views/header/Header.tsx
+++ src/views/header/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt, faSignInAlt, faUser } from '@fortawesome/free-solid-svg-icons'
 import { userSelector } from '../../auth/authSelector'
 import { Link } from 'react-router-dom'
 import useThunkDispatch from '../../common/useThunkDispatch'
@@ -9,18 +11,32 @@ const Header: React.FC = () => {
   const user = useSelector(userSelector)
   const dispatch = useThunkDispatch()
   return (
-    <header>
-      {user ? (
-        <div>
-          {user.displayName}
-          <button onClick={() => dispatch(requestLogout())}>Log out</button>
+    <div className="container">
+      <header className="header">
+        <Link to="/" className="header_logo">
+          <div className="header_logo-left">Travel</div>
+          <div className="header_logo-right">Planner</div>
+        </Link>
+
+        <div className="profile">
+          {user ? (
+            <>
+              <div className="profile_user">
+                <FontAwesomeIcon className="profile_user-icon" icon={faUser} />
+                {user.displayName}
+              </div>
+              <button className="profile_logout" onClick={() => dispatch(requestLogout())}>
+                <FontAwesomeIcon icon={faSignOutAlt} />
+              </button>
+            </>
+          ) : (
+            <Link to="/auth/signin">
+              Sign In / Sign Up
+            </Link>
+          )}
         </div>
-      ) : (
-        <div>
-          <Link to="/auth/signin">Login Me</Link>
-        </div>
-      )}
-    </header>
+      </header>
+    </div>
   )
 }
 
