5c5621d20055c1a8a91ea95cb8ee7d273a352d7e
Evgeniy Bulgakov <zoom81@yandex.ru>
2019-09-30 19:08:09 +0400

    Add return types for createTypedStructuredSelector

--- src/common/createTypedStructuredSelector.ts
+++ src/common/createTypedStructuredSelector.ts
@@ -1,5 +1,6 @@
 import { createStructuredSelector, Selector } from 'reselect'
 import { Store } from '../types/common'
 
-export default <ISelectedProps>(selectors: { [K in keyof ISelectedProps]: Selector<Store, ISelectedProps[K]> }) =>
-  createStructuredSelector<Store, ISelectedProps>(selectors)
+export default <ISelectedProps>(
+  selectors: { [K in keyof ISelectedProps]: Selector<Store, ISelectedProps[K]> }
+): Selector<Store, ISelectedProps> => createStructuredSelector<Store, ISelectedProps>(selectors)
