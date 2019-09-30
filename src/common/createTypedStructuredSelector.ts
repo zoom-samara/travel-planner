cb898be195d2e3c0ea4675805dab05079205f551
Evgeniy Bulgakov <zoom81@yandex.ru>
2019-09-30 23:27:33 +0400

    Rename variables

--- src/common/createTypedStructuredSelector.ts
+++ src/common/createTypedStructuredSelector.ts
@@ -1,6 +1,6 @@
 import { createStructuredSelector, Selector } from 'reselect'
 import { Store } from '../types/common'
 
-export default <ISelectedProps>(
-  selectors: { [K in keyof ISelectedProps]: Selector<Store, ISelectedProps[K]> }
-): Selector<Store, ISelectedProps> => createStructuredSelector<Store, ISelectedProps>(selectors)
+export default <SelectedProps>(
+  selectors: { [K in keyof SelectedProps]: Selector<Store, SelectedProps[K]> }
+): Selector<Store, SelectedProps> => createStructuredSelector<Store, SelectedProps>(selectors)
